e7730663c960f69b61d4bee7ff4613a1039f4be8
C-Y, Liao <csvbc0509@gmail.com>
2026-01-09 01:18:53 +0800

    FIXED updateAEDdata.py add header to avoid timeout

--- updateAEDdata.py
+++ updateAEDdata.py
@@ -8,6 +8,13 @@ def download_csv():
     url = "https://tw-aed.mohw.gov.tw/openData?t=csv"
     print(f"正在下載資料: {url} ...")
     
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Referer": "https://tw-aed.mohw.gov.tw/"
+    }
+    
     try:
         response = requests.get(url, verify=False)
         response.raise_for_status()
